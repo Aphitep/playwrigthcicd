382503f8fca23c37f5c6259e81bc208b8d41b60d
aumaphitep <aumaphitep@gmail.com>
2024-09-23 11:05:10 +0700

    first commit

---
+++ tests/api.spec.js
@@ -0,0 +1,26 @@
+const { test, expect } = require('@playwright/test');
+
+var user_id ;
+test('get user',async ({ request }) => {
+    const response = await request.get('https://reqres.in/api/users');
+
+    expect(response.status()).toBe(200);
+})
+test('create user',async ({ request }) => {
+    const response = await request.post('https://reqres.in/api/users',
+    {
+        data:{"name":"morpheus","job":"leader"},
+        headers:{"Accept":"application/json"}
+    });
+
+    expect(response.status()).toBe(201);
+
+    var res = await response.json();
+    user_id = res.id;
+})
+
+test('delete user',async ({ request }) => {
+    const response = await request.delete('https://reqres.in/api/users/'+user_id,);
+
+    expect(response.status()).toBe(204);
+})
